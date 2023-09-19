ec20feaf84bcf258d94b1cfc38258823e09bcc11
Liam Arguedas <iliamftw2013@gmail.com>
2023-09-19 18:55:20 -0300

    Update (Classification)

--- supervised/_models.py
+++ supervised/_models.py
@@ -6,24 +6,35 @@ import numpy as np
 import sklearn, xgboost, lightgbm, catboost
 
 class Classification():
-    def __init__(self, random_state = None, n_jobs = -1):
-        self.random_state = random_state
-        self.n_jobs = n_jobs
-    
-    def set_random_state(self, random_state):
-        """      
-        Controls both the randomness of the algorithms and reproducibility of the results
+    def __init__(self, multi_class = None, random_state = None, n_jobs = None, verbose = True):
+        """
+        Summarizes variety of Classification Machine Learning models into one instance.
         
         Parameters
         ----------
-        random_state: int or None, default = None
+        multi_class: {"ovr", "ovo", "auto"} or None, default = None
+            Categories (unique values) per feature:
+            
+            - None : Indicates that the problem requires a binary classification, and there is no need for multiclass functionality.
+            - "ovr" : Each class is treated as a binary target while the rest are treated as the other class. 
+            - "ovo" : The classifier is trained for each pair of classes to determine the class with the most votes from pairwise comparisons.
+            
+        random_state: int, default = None
+            Controls the seed for generating random numbers, ensuring reproducibility in random processes such as data shuffling or initializations.
+        
+        n_jobs: int, default = None
+            The number of jobs to run in parallel. fit, predict, decision_path and apply are all parallelized over the trees.
+            None means 1 unless in a joblib.parallel_backend context. -1 means using all processors.
+        
+        verbose: bool, default = True
+            Controls the extralearning verbosity when fitting and predicting. 
+            Note: All estimator verbose are set to 0 or False, meaning no output other than extralearning information.
         """
-        
-        if not isinstance(random_state, (int, None)):
-            raise TypeError(f"random_state must be int or None, not {type(random_state)}.")
-        
+        self.multi_class = multi_class
         self.random_state = random_state
-        
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+            
     def return_pandas(self, pandas = True):
         """
         The default output in the form of a pandas DataFrame or pandas Series, depending of the return.
@@ -36,30 +47,38 @@ class Classification():
         if not isinstance(pandas, bool)):
             raise TypeError(f"Parameter must be Bool, not {type(pandas)}.")
         
-        self.pandas = pandas
-    
-    def __binary__estimators(self):
+        self.__pandas = pandas
+   
+    def __estimators(self):
         
-        return {"Logistic Regression":sklearn.linear_model.LogisticRegression(random_state = self.random_state),
-        "Decision Tree":sklearn.tree.DecisionTreeClassifier(random_state = self.random_state),
-        "Random Forest":sklearn.ensemble.RandomForestClassifier(random_state = self.random_state),
-        "Linear Support Vector Machine":sklearn.svm.SVC(random_state = self.random_state, probability = True, kernel = "linear"),
-        "Linear Support Vector Machine":sklearn.svm.SVC(random_state = self.random_state, probability = True, kernel = "rbf"),
-        "K-nearest neighbors":sklearn.neighbors.KNeighborsClassifier(),
-        "Naive Bayes":sklearn.naive_bayes.GaussianNB(),
-        "Gradient Boosting":sklearn.ensemble.GradientBoostingClassifier(random_state = self.random_state),
-        "XGBoost":xgboost.XGBClassifier(random_state = self.random_state),
-        "LightGBM":lightgbm.LGBMClassifier(random_state = self.random_state, verbose = -1),
-        "CatBoost":catboost.CatBoostClassifier(random_state = self.random_state, verbose = False),
-        "AdaBoost":sklearn.ensemble.AdaBoostClassifier(random_state = self.random_state),
-        "Linear Discriminant":sklearn.discriminant_analysis.LinearDiscriminantAnalysis(),
-        "Quadratic Discriminant":sklearn.discriminant_analysis.QuadraticDiscriminantAnalysis(),
-        "Extra Trees":sklearn.ensemble.ExtraTreesClassifier(random_state = self.random_state),
-        "Gaussian Process":sklearn.gaussian_process.GaussianProcessClassifier(random_state = self.random_state),
-        "Multilayer perceptron":sklearn.neural_network.MLPClassifier(random_state = self.random_state)}
+        if self.multi_class is None: 
             
-    def __multiclass__estimators(self):
-        pass
+            self.estimators = {
+                "Logistic Regression":sklearn.linear_model.LogisticRegression(random_state = self.random_state, n_jobs = self.n_jobs),
+                "Decision Tree":sklearn.tree.DecisionTreeClassifier(random_state = self.random_state, n_jobs = self.n_jobs),
+                "Random Forest":sklearn.ensemble.RandomForestClassifier(random_state = self.random_state, n_jobs = self.n_jobs),
+                "Linear Support Vector Machine":sklearn.svm.SVC(random_state = self.random_state, probability = True, kernel = "linear", n_jobs = self.n_jobs),
+                "Linear Support Vector Machine":sklearn.svm.SVC(random_state = self.random_state, probability = True, kernel = "rbf", n_jobs = self.n_jobs),
+                "K-nearest neighbors":sklearn.neighbors.KNeighborsClassifier(n_jobs = self.n_jobs),
+                "Naive Bayes":sklearn.naive_bayes.GaussianNB(n_jobs = self.n_jobs),
+                "Gradient Boosting":sklearn.ensemble.GradientBoostingClassifier(random_state = self.random_state, n_jobs = self.n_jobs),
+                "XGBoost":xgboost.XGBClassifier(random_state = self.random_state, nthread = self.n_jobs),
+                "LightGBM":lightgbm.LGBMClassifier(random_state = self.random_state, verbose = -1, n_jobs = self.n_jobs),
+                "CatBoost":catboost.CatBoostClassifier(random_state = self.random_state, verbose = False, thread_count = self.n_jobs),
+                "AdaBoost":sklearn.ensemble.AdaBoostClassifier(random_state = self.random_state, n_jobs = self.n_jobs),
+                "Linear Discriminant":sklearn.discriminant_analysis.LinearDiscriminantAnalysis(n_jobs = self.n_jobs),
+                "Quadratic Discriminant":sklearn.discriminant_analysis.QuadraticDiscriminantAnalysis(n_jobs = self.n_jobs),
+                "Extra Trees":sklearn.ensemble.ExtraTreesClassifier(random_state = self.random_state, n_jobs = self.n_jobs),
+                "Gaussian Process":sklearn.gaussian_process.GaussianProcessClassifier(random_state = self.random_state, n_jobs = self.n_jobs),
+                "Multilayer perceptron":sklearn.neural_network.MLPClassifier(random_state = self.random_state, n_jobs = self.n_jobs)
+                }
+        else:
+            self.estimators = {
+                
+            }          
+    
+    def add_estimator(self, estimator: tuple):
+        if estimator not in self.__binary__estimators
     
     def get_params(self):
         pass
