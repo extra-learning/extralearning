023b735194a756343790172a57e6eecb26444bf4
Liam Arguedas <iliamftw2013@gmail.com>
2023-09-21 19:42:40 -0300

    Update (Classification)

--- supervised/_models.py
+++ supervised/_models.py
@@ -1,9 +1,25 @@
 # Authors: Liam Arguedas <iliamftw2013@gmail.com>
 # License: BSD 3 clause
 
+import abc
 import pandas as pd
 import numpy as np
-import sklearn, xgboost, lightgbm, catboost
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import GradientBoostingClassifier
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.neural_network import MLPClassifier
 
 class Classification():
     def __init__(self, multi_class = None, random_state = None, n_jobs = None, verbose = True):
@@ -40,6 +56,25 @@ class Classification():
         self.random_state = random_state
         self.n_jobs = n_jobs
         self.verbose = verbose
+        self.__estimators = [
+                ("Logistic Regression", LogisticRegression(random_state = self.random_state, n_jobs = self.n_jobs)),
+                ("Decision Tree", DecisionTreeClassifier(random_state = self.random_state)),
+                ("Random Forest", RandomForestClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
+                ("Linear Support Vector Machine", SVC(random_state = self.random_state, probability = True, kernel = "linear")),
+                ("RBF Support Vector Machine", SVC(random_state = self.random_state, probability = True, kernel = "rbf")),
+                ("K-nearest neighbors", KNeighborsClassifier(n_jobs = self.n_jobs)),
+                ("Naive Bayes", GaussianNB()),
+                ("Gradient Boosting", GradientBoostingClassifier(random_state = self.random_state)),
+                ("XGBoost", XGBClassifier(random_state = self.random_state, nthread = self.n_jobs)),
+                ("LightGBM", LGBMClassifier(random_state = self.random_state, verbose = -1, n_jobs = self.n_jobs)),
+                ("CatBoost", CatBoostClassifier(random_state = self.random_state, verbose = False, thread_count = self.n_jobs)),
+                ("AdaBoost", AdaBoostClassifier(random_state = self.random_state)),
+                ("Linear Discriminant", LinearDiscriminantAnalysis()),
+                ("Quadratic Discriminant", QuadraticDiscriminantAnalysis()),
+                ("Extra Trees", ExtraTreesClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
+                ("Gaussian Process", GaussianProcessClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
+                ("Multilayer perceptron", MLPClassifier(random_state = self.random_state))
+            ]
             
     def return_pandas(self, pandas = True) -> None:
         """
@@ -55,32 +90,11 @@ class Classification():
         
         self.__pandas = pandas
    
-    def __estimators(self) -> None:
-        
-        if self.multi_class is None: 
-            
-            self._estimators = [
-                ("Logistic Regression",sklearn.linear_model.LogisticRegression(random_state = self.random_state, n_jobs = self.n_jobs)),
-                ("Decision Tree",sklearn.tree.DecisionTreeClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
-                ("Random Forest",sklearn.ensemble.RandomForestClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
-                ("Linear Support Vector Machine",sklearn.svm.SVC(random_state = self.random_state, probability = True, kernel = "linear", n_jobs = self.n_jobs)),
-                ("RBF Support Vector Machine",sklearn.svm.SVC(random_state = self.random_state, probability = True, kernel = "rbf", n_jobs = self.n_jobs)),
-                ("K-nearest neighbors",sklearn.neighbors.KNeighborsClassifier(n_jobs = self.n_jobs)),
-                ("Naive Bayes",sklearn.naive_bayes.GaussianNB(n_jobs = self.n_jobs)),
-                ("Gradient Boosting",sklearn.ensemble.GradientBoostingClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
-                ("XGBoost",xgboost.XGBClassifier(random_state = self.random_state, nthread = self.n_jobs)),
-                ("LightGBM",lightgbm.LGBMClassifier(random_state = self.random_state, verbose = -1, n_jobs = self.n_jobs)),
-                ("CatBoost",catboost.CatBoostClassifier(random_state = self.random_state, verbose = False, thread_count = self.n_jobs)),
-                ("AdaBoost",sklearn.ensemble.AdaBoostClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
-                ("Linear Discriminant",sklearn.discriminant_analysis.LinearDiscriminantAnalysis(n_jobs = self.n_jobs)),
-                ("Quadratic Discriminant",sklearn.discriminant_analysis.QuadraticDiscriminantAnalysis(n_jobs = self.n_jobs)),
-                ("Extra Trees",sklearn.ensemble.ExtraTreesClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
-                ("Gaussian Process",sklearn.gaussian_process.GaussianProcessClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
-                ("Multilayer perceptron",sklearn.neural_network.MLPClassifier(random_state = self.random_state, n_jobs = self.n_jobs))
-            ]
-    
     def get_estimators(self) -> list:
-        return self._estimators
+        """
+        Returns a list of tuples containing the current estimators.
+        """
+        return self.__estimators
         
     def add_estimator(self, estimator: tuple):
         """
@@ -101,11 +115,66 @@ class Classification():
         
         Note: To check the default estimators, you can use the `.get_estimators()` method.
         """
-        
+         
         if self.multi_class is None:
-            self.estimators.append(estimator)
+            self.__estimators.append(estimator)
             
         else:
             pass # PENDING
 
+    def reset_estimators(self):
+        "Use to reset the estimators to be used to the original list"
+        self.__estimators = [
+                ("Logistic Regression", LogisticRegression(random_state = self.random_state, n_jobs = self.n_jobs)),
+                ("Decision Tree", DecisionTreeClassifier(random_state = self.random_state)),
+                ("Random Forest", RandomForestClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
+                ("Linear Support Vector Machine", SVC(random_state = self.random_state, probability = True, kernel = "linear")),
+                ("RBF Support Vector Machine", SVC(random_state = self.random_state, probability = True, kernel = "rbf")),
+                ("K-nearest neighbors", KNeighborsClassifier(n_jobs = self.n_jobs)),
+                ("Naive Bayes", GaussianNB()),
+                ("Gradient Boosting", GradientBoostingClassifier(random_state = self.random_state)),
+                ("XGBoost", XGBClassifier(random_state = self.random_state, nthread = self.n_jobs)),
+                ("LightGBM", LGBMClassifier(random_state = self.random_state, verbose = -1, n_jobs = self.n_jobs)),
+                ("CatBoost", CatBoostClassifier(random_state = self.random_state, verbose = False, thread_count = self.n_jobs)),
+                ("AdaBoost", AdaBoostClassifier(random_state = self.random_state)),
+                ("Linear Discriminant", LinearDiscriminantAnalysis()),
+                ("Quadratic Discriminant", QuadraticDiscriminantAnalysis()),
+                ("Extra Trees", ExtraTreesClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
+                ("Gaussian Process", GaussianProcessClassifier(random_state = self.random_state, n_jobs = self.n_jobs)),
+                ("Multilayer perceptron", MLPClassifier(random_state = self.random_state))
+            ]
+    
+    def get_params(self):
+        """Returns a list of tuples containing the estimator and their default parameters"""
+        return [(estimator[1], estimator[1].get_params()) for estimator in self.__estimators]   
+        
+    def pass_params(self, estimator, overwrite = True):
+        """
+        Parameters
+        ----------
+        estimator: tuple, (estimator, params)
+            - estimator: A classifier that implements `.fit()`, `.predict()` and `.predict_proba()` if available.
+            - params: A dictionary containing the params.
+            
+        overwrite: bool, default = True
+            If `True` it will overwrite the current estimator in list, if `False`, method `.add_estimator()` will be called 
+            and both estimators are going to be kept.
+            
+        Examples
+        --------
+        >>> from extralearning import Classification
+            
+        >>> model = Classification()
+        
+        >>> model.pass_params(estimator = (RandomForestClassifier, {"n_estimators":300, "criterion":"gini"}))
+        """
+        if not overwrite:
+            self.add_estimator((str(type(estimator[0]())), estimator[0](**estimator[1])))
+            
+        else:
+            pass # PENDING
+        
+        
+        
+        
         
